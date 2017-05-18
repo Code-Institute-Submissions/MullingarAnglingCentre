a67edd926347a9155c42389b4e581387373733d3
Martin Allard <Martin Allard>
2017-05-18 10:41:32 +0100

    spacing issue fixed in footer

--- js/moredetails.js
+++
@@ -1,6 +0,0 @@
-
-	/*var textareaWidth = document.getElementsByClassName("textarea").scrollWidth;
-
-	// width of our wrapper equals width of the inner part of the textarea
-	document.getElementsByClassName("wrapper").style.width = textareaWidth + '%';
-*/
--- js/moredetails2.js
+++
@@ -1,8 +0,0 @@
-function changec() {
-       var xDiv = document.getElementById('coding');
-
-       if (xDiv.style.height == '') 
-           xDiv.style.height = '180px'
-       else 
-           xDiv.style.height = ''
-}
